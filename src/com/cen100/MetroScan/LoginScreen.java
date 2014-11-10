b5eb8f9f25696e7a1d3de03083e99b6e1e04000b
heroictenshi <heroictenshi@users.noreply.github.com>
2014-11-10 16:28:07 -0500

    Initial Project created

---
+++ src/com/cen100/MetroScan/LoginScreen.java
@@ -0,0 +1,17 @@
+package com.cen100.MetroScan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jeffry Lien on 10/11/2014.
+ */
+public class LoginScreen extends Activity {
+
+    @Override
+    public void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginscreen);
+    }
+}
